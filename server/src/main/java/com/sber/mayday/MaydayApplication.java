5ea00e60e3442200f9a39a9715e07e410a985711
Vlad Zotov <sotowwlad@gmail.com>
2021-02-27 11:55:52 +0300

    add started server

---
+++ server/src/main/java/com/sber/mayday/MaydayApplication.java
@@ -0,0 +1,13 @@
+package com.sber.mayday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaydayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaydayApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/sber/mayday/MaydayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sber.mayday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaydayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

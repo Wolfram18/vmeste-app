7a3f3b8f6fcfefa43c9b90c01f20ef5fd9c9a7d9
vagne0rlove9 <kirik-pos@mail.ru>
2021-02-27 11:35:53 +0300

    add post

---
+++ Sber/Sber/ClientApp/src/components/Home/Home.js
@@ -0,0 +1,63 @@
+﻿import React, { Component } from 'react';
+import './Home.css';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+export class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tags: ["Нейросети", "Web разработка", "IOS разработка", "AV/VR разработка"]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h3 className="header">Создание поста</h3>
+                <div className="container-create-post">
+                    <h5>ВЫБЕРИТЕ ТИП ОСНОВНОГО КОНТЕНТА</h5>
+                    <div className="container-content">
+                        <div className="content-type-video">
+                            ВИДЕО
+                        </div>
+                        <div className="content-type-audio">
+                            АУДИО   
+                        </div>
+                        <div className="content-type-text">
+                            ТЕКСТ
+                        </div>
+                        <div className="content-type-img">
+                            ИЗОБРАЖЕНИЕ
+                        </div>
+                    </div>
+                    <div className="container-tags">
+                        <Autocomplete
+                            multiple
+                            id="tags-outlined"
+                            options={this.state.tags}
+                            getOptionLabel={(option) => option}
+                            
+                            filterSelectedOptions
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    variant="outlined"
+                                    label="Тэги"
+                                    placeholder="Тэги"
+                                />
+                            )}
+                        />
+                    </div>
+                    <div className="container-button-create">
+                        <Button className="button-create-post" variant="contained" color="primary">
+                            создать пост
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- Sber/Sber/ClientApp/src/index.js
+++ Sber/Sber/ClientApp/src/index.js
@@ -4,15 +4,33 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#27678d',
+            light: '#666ad1',
+            dark: '#001970',
+        },
+        secondary: {
+            main: '#FF355D',
+            light: '#26c6da',
+            dark: '#0095a8',
+        },
+    }
+});
+
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+    <BrowserRouter basename={baseUrl}>
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
+    </BrowserRouter>,
+    rootElement);
 
 registerServiceWorker();
 
